2884f6b753e61185db119835065e1afdd6477870
amyjprogrammer <amyjprogrammer@gmail.com>
2021-10-11 16:29:19 -0400

    Api

---
+++ 10_IntroToAPI/Program.cs
@@ -0,0 +1,69 @@
+﻿using _10_IntroToAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_IntroToAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating an http client to query APIs
+            HttpClient httpClient = new HttpClient();
+
+            //creating a task
+            Task<HttpResponseMessage> responseTask = httpClient.GetAsync("https://swapi.dev/api/people/1");
+
+            //getting a response from the task
+            HttpResponseMessage response = responseTask.Result;
+
+            //creating and getting the response
+            HttpResponseMessage responseTwo = httpClient.GetAsync("https://swapi.dev/api/people/2").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string stringResponse = response.Content.ReadAsStringAsync().Result;//gives the full json object
+                //Console.WriteLine(stringResponse);
+
+                Person person = response.Content.ReadAsAsync<Person>().Result;//serializes the data for us
+
+                Console.WriteLine("Name: " + person.Name);
+                Console.WriteLine("Height: " + person.Height);
+                Console.WriteLine("Hair Color: "+ person.HairColor);
+
+                foreach (var vehicleUri in person.Vehicles)
+                {
+                    HttpResponseMessage vehicleResponse = httpClient.GetAsync(vehicleUri).Result;
+                    Vehicle vehicle = vehicleResponse.Content.ReadAsAsync<Vehicle>().Result;
+                    Console.WriteLine("Vehicle: "+ vehicle.Name);
+                }
+            }
+
+            SWAPIService service = new SWAPIService();
+            Person personTwo = service.GetPersonAsync("https://swapi.dev/api/people/5").Result;
+
+            if(personTwo != null)
+            {
+                Console.WriteLine(personTwo.Name);
+
+                foreach (var vehicleUri in personTwo.Vehicles)
+                {
+                    Vehicle vehicle = service.GetVehicleAsync(vehicleUri).Result;
+                    Console.WriteLine(vehicle.Name);
+                }
+                foreach (var filmUri in personTwo.Films)
+                {
+                    Film film = service.GetAsync<Film>(filmUri).Result;
+                    Console.WriteLine(film.Title);
+                }
+            }
+
+            Person personThree = service.GetAsync<Person>("https://swapi.dev/api/people/12").Result;
+            Console.WriteLine(personThree.Name);
+        }
+    }
+}
---
+++ 10_IntroToAPI/SWAPIService.cs
@@ -0,0 +1,55 @@
+﻿using _10_IntroToAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_IntroToAPI
+{
+    class SWAPIService
+    {
+        //field
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public async Task<Person> GetPersonAsync(string uri)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync(uri);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Person person = await response.Content.ReadAsAsync<Person>();
+                return person;
+            }
+            //If not successful
+            return null;
+        }
+
+        public async Task<Vehicle> GetVehicleAsync(string uri)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync(uri);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Vehicle vehicle = await response.Content.ReadAsAsync<Vehicle>();
+                return vehicle;
+            }
+            //If not successful
+            return null;
+        }
+
+        //Generics
+        public async Task<T> GetAsync<T>(string uri)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync(uri);
+
+            if (response.IsSuccessStatusCode)
+            {
+                T obj = await response.Content.ReadAsAsync<T>();
+                return obj;
+            }
+            return default;
+        }
+    }
+}
